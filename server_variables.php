ac7f38b73a974d583904672d642c4bab695cd899
Vickie Scullard <vickie.scullard@gmail.com>
2022-09-06 16:31:44 +0100

    server and session variables pages

---
+++ server_variables.php
@@ -0,0 +1,12 @@
+<?php
+
+# superglobal server array
+
+// foreach($_SERVER as $k => $v) {
+//     echo $k . ' - ' . $v . '<br/>';
+// }
+
+echo "<pre>";
+print_r($_SERVER);
+
+?>
---
+++ session_variables.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+$_SESSION['browser'] = $_SERVER['HTTP_USER_AGENT'];
+$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+$_SESSION['time'] = time();
+echo "<pre>";
+print_r($_SESSION);
+
+echo "<br><br>";
+phpinfo();
+
+?>
